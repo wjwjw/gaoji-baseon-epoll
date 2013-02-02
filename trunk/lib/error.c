eb5cdaff24e286ec303d24f473aac16f3c7ca0c0
china.key.wjw@gmail.com <china.key.wjw@gmail.com@a9bc3994-0fc0-bb09-d693-db44677b3986>
2013-02-02 11:55:52 +0000

    error.c:102:3: 警告： 格式字符串不是一个字面字符串而且没有待格式化的实参

--- trunk/lib/error.c
+++ trunk/lib/error.c
@@ -14,7 +14,7 @@ void
 err_ret(const char *fmt, ...)
 {
 	va_list		ap;
-
+    
 	va_start(ap, fmt);
 	err_doit(1, LOG_INFO, fmt, ap);
 	va_end(ap);
@@ -28,7 +28,7 @@ void
 err_sys(const char *fmt, ...)
 {
 	va_list		ap;
-
+    
 	va_start(ap, fmt);
 	err_doit(1, LOG_ERR, fmt, ap);
 	va_end(ap);
@@ -42,7 +42,7 @@ void
 err_dump(const char *fmt, ...)
 {
 	va_list		ap;
-
+    
 	va_start(ap, fmt);
 	err_doit(1, LOG_ERR, fmt, ap);
 	va_end(ap);
@@ -57,7 +57,7 @@ void
 err_msg(const char *fmt, ...)
 {
 	va_list		ap;
-
+    
 	va_start(ap, fmt);
 	err_doit(0, LOG_INFO, fmt, ap);
 	va_end(ap);
@@ -71,7 +71,7 @@ void
 err_quit(const char *fmt, ...)
 {
 	va_list		ap;
-
+    
 	va_start(ap, fmt);
 	err_doit(0, LOG_ERR, fmt, ap);
 	va_end(ap);
@@ -86,7 +86,7 @@ err_doit(int errnoflag, int level, const char *fmt, va_list ap)
 {
 	int		errno_save, n;
 	char	buf[MAXLINE + 1];
-
+    
 	errno_save = errno;		/* value caller might want printed */
 #ifdef	HAVE_VSNPRINTF
 	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
@@ -97,9 +97,9 @@ err_doit(int errnoflag, int level, const char *fmt, va_list ap)
 	if (errnoflag)
 		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
 	strcat(buf, "\n");
-
+    
 	if (daemon_proc) {
-		syslog(level, buf);
+		syslog(level,"%s", buf);
 	} else {
 		fflush(stdout);		/* in case stdout and stderr are the same */
 		fputs(buf, stderr);
